0fa76a5d04367f8db59249ad46e37c27f50a0ae8
florin.asavei <florin.asavei@exghts.com>
2022-11-28 10:55:57 +0200

    Added custom generic list

---
+++ plainCSharp/CustomGenericList.Tests/GenericList/GenericCustomListShould.cs
@@ -0,0 +1,87 @@
+﻿using CustomGenericList.CustomLists.DTOS;
+using CustomGenericList.CustomLists.GenericList;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomGenericList.Tests.GenericList
+{
+    public class GenericList_Student_Should
+    {
+        [Fact]
+        public void GenericList_Should_Return_Length_Of_Elements()
+        {
+            var chris = new Student { Id = 10, Name = "Chris", };
+            var students = new GenericCustomList<Student>
+            {
+                chris,
+                new Student{ Id= 20, Name = "Florin"},
+                new Student{ Id= 30, Name = "Gigi" }
+            };
+
+            students.Count.ShouldBe(3);
+        }
+
+
+        [Fact]
+        public void GenericList_Should_Contain_Added_Element()
+        {
+            var chris = new Student { Id = 10, Name = "Chris", };
+            var students = new GenericCustomList<Student>
+            {
+                chris,
+                new Student{ Id= 20, Name = "Florin"},
+                new Student{ Id= 30, Name = "Gigi" }
+            };
+
+            students.Contains(chris).ShouldBeTrue();
+        }
+
+        [Fact]
+        public void GenericList_Remove_Sould_Remove_Item()
+        {
+
+            var chris = new Student { Id = 10, Name = "Chris", };
+            var students = new GenericCustomList<Student>
+            {
+                chris,
+                new Student{ Id= 20, Name = "Florin"},
+                new Student{ Id= 30, Name = "Gigi" }
+            };
+
+            students.Remove(chris).ShouldBeTrue();
+            students.ShouldNotContain(chris);
+        }
+
+
+        /// <summary>
+        /// This test fails for now
+        /// </summary>
+        [Fact]
+        public void GenericList_Sould_Iterate()
+        {
+
+            var chris = new Student { Id = 10, Name = "Chris", };
+            var students = new GenericCustomList<Student>
+            {
+                chris,
+                new Student{ Id= 20, Name = "Florin"},
+                new Student{ Id= 30, Name = "Gigi" }
+            };
+
+            var interations = 0;
+            foreach (var p in students)
+            {
+                interations++;
+            }
+
+            interations.ShouldBeEquivalentTo(students.Count);
+        }
+
+
+    }
+}
---
+++ plainCSharp/CustomGenericList/CustomLists/GenericList/GenericCustomList.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections;
+
+namespace CustomGenericList.CustomLists.GenericList
+{
+    public class GenericCustomList<T> : IList<T>
+    {
+
+        private T[] _elements = new T[8];
+        private int _count;
+
+        public GenericCustomList()
+        {
+            _count = 0;
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                return _elements[index];
+            }
+            set
+            {
+                _elements[index] = value;
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return _count;
+            }
+        }
+
+        //skip
+        public bool IsReadOnly => throw new NotImplementedException();
+
+
+        public void Add(T item)
+        {
+            if (_count < _elements.Length)
+            {
+                _elements[_count] = item;
+                _count++;
+            }
+            else
+            {
+                // increase lengths
+            }
+        }
+
+        public void Clear()
+        {
+            _count = 0;
+        }
+
+        public bool Contains(T item)
+        {
+            if(_elements == null || _elements.Length == 0 || item == null)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < Count; i++)
+            {
+                if(_elements[i] == null)
+                {
+                    return false;
+                }
+
+                if (_elements[i].Equals(item))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        //skip
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                array.SetValue(_elements[i], arrayIndex++);
+            }
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            return (IEnumerator<T>)_elements.GetEnumerator();
+        }
+
+        // TODO: fix enumarator
+        public IEnumerator GetEnumerator()
+        {
+            return _elements.GetEnumerator();
+        }
+
+        public int IndexOf(T item)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (_elements[i].Equals(item))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public void Insert(int index, T item)
+        {
+            if ((_count + 1 <= _elements.Length) && (index < Count) && (index >= 0))
+            {
+                _count++;
+
+                for (int i = Count - 1; i > index; i--)
+                {
+                    _elements[i] = _elements[i - 1];
+                }
+                _elements[index] = item;
+            }
+        }
+
+        public bool Remove(T item)
+        {
+            if (IndexOf(item) > -1)
+            {
+                RemoveAt(IndexOf(item));
+                return true;
+            }
+
+            return false;
+        }
+
+        public void RemoveAt(int index)
+        {
+            if ((index >= 0) && (index < Count))
+            {
+                for (int i = index; i < Count - 1; i++)
+                {
+                    _elements[i] = _elements[i + 1];
+                }
+                _count--;
+            }
+        }
+
+    }
+}
